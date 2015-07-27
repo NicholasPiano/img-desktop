57942a45edb1dbbe385b909a8a908e18814e3053
NicholasPiano <nicholas.d.piano@gmail.com>
2015-07-27 13:55:22 +0100

    trying basic erode first
    No energy functions, no profiles.

--- protrusion-test/test_custom_erode.py
+++ protrusion-test/test_custom_erode.py
@@ -13,29 +13,21 @@ from mod_base import *
 # results:
 
 # paths
-zcomp_segmented = load(join(t_path, 'base_segmentation', 'results'), 'zcomp.tiff')
+test_path = join(t_path, 'custom_erode')
+marker = load(join(test_path, 'marker.png'))
+zcomp_cp = load(join(test_path, 'zcomp_cp.tiff'))
+zdiff_cp = load(join(test_path, 'zdiff_cp.tiff'))
 
-# cut and rescale zmod to size of environment
-cut_zmod = (cut(zmod) * 98).astype(int)
-masked_zmod = np.ma.array(cut_zmod, mask=(zcomp_segmented!=1.0), fill_value=0)
-
-# get levels inside mask
-levels = [l for l in np.unique(masked_zmod.filled()) if l!=0]
-z_diff = np.zeros(cut_zmod.shape)
-
-for level in levels:
-  level_z_diff = np.abs(np.ones(cut_zmod.shape) * level - cut_zmod)
-  level_z_diff = 1.0 - exposure.rescale_intensity(level_z_diff * 1.0)
-  level_z_diff = gf(level_z_diff, sigma=5)
-
-  z_diff += level_z_diff.copy()
+# TEST 1: erode zdiff_cp until it touches zcomp_cp
+while not_touching(zdiff_cp, zcomp_cp):
+  erode(zdiff_cp)
 
 # cut and modify zbf
-cut_zbf = cut(zbf)
+# cut_zbf = cut(zbf)
 
-mod = cut_zbf * z_diff
+# mod = cut_zbf * z_diff
 
 # plt.imshow(mod, cmap='Greys_r')
 # plt.show()
 
-imsave(join(t_path, 'segmented_mask_z', 'zdiff.png'), mod)
+# imsave(join(t_path, 'segmented_mask_z', 'zdiff.png'), mod)
