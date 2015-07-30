e0d48b701c436da5fab5664762b121614d682815
NicholasPiano <nicholas.d.piano@gmail.com>
2015-07-30 16:52:46 +0100

    writing detailed notes

---
+++ basic_python_script/test.py
@@ -0,0 +1,2 @@
+# The purpose of this script is to test compilation of a basic python program on windows. The most reliable way
+# is to write a string to a file. The command line will be suppressed during compilation.
