10886c844511d3f62d5e5fc74398ab1b8a871660
NicholasPiano <nicholas.d.piano@gmail.com>
2015-07-31 01:02:34 +0100

    basic python script done

--- basic_python_script/test.py
+++ basic_python_script/test.py
@@ -1,2 +1,10 @@
 # The purpose of this script is to test compilation of a basic python program on windows. The most reliable way
 # is to write a string to a file. The command line will be suppressed during compilation.
+
+import os
+import datetime
+
+self_path = os.path.dirname(os.path.realpath(__file__))
+
+with open(os.path.join(self_path, 'test.txt'), 'w+') as f:
+  f.write(str(datetime.datetime.now()) + '\n')
