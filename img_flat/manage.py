85dcf1411ff69fe4476cc9832e51cafa57e706fb
NicholasPiano <nicholas.d.piano@gmail.com>
2015-08-02 04:27:49 +0100

    trying syncdb

--- img_flat/manage.py
+++ img_flat/manage.py
@@ -15,6 +15,10 @@ if __name__ == "__main__":
   os.environ["DJANGO_SETTINGS_MODULE"] = "woot.settings"
   import django.contrib.admin.apps
   from expt.exec import run
-  run()
+
+  from django.core.management import call_command
+  call_command('syncdb', interactive=False)
+
+  # run()
   # from django.core.management import execute_from_command_line
   # execute_from_command_line(sys.argv)
