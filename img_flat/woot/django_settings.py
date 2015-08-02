a19866049e9239879ad0e0b4f96dc30b55203e19
NicholasPiano <nicholas.d.piano@gmail.com>
2015-08-02 03:53:06 +0100

    adding the rest of the default apps so it won't complain

--- img_flat/woot/django_settings.py
+++ img_flat/woot/django_settings.py
@@ -14,6 +14,12 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DJANGO_APPS = (
+  'django.contrib.auth',
+  'django.contrib.contenttypes',
+  'django.contrib.sessions',
+  'django.contrib.sites',
+  'django.contrib.messages',
+  'django.contrib.staticfiles',
   'django.contrib.admin',
 )
 
