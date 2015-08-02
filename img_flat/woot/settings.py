8cd3e51ddec3a9722cdc492a19990eb20fbf59cd
NicholasPiano <nicholas.d.piano@gmail.com>
2015-08-02 03:33:55 +0100

    test img_flat

---
+++ img_flat/woot/settings.py
@@ -0,0 +1,35 @@
+
+########## IMPORT
+import os
+from datetime import timedelta
+from os import mkdir
+from os.path import abspath, basename, dirname, join, normpath, exists
+from sys import path
+import string
+import json
+import sys
+
+from django_settings import *
+########## IMPORT
+
+
+########## PATH CONFIGURATION
+# Absolute filesystem path to the Django project directory:
+DJANGO_ROOT = dirname(abspath(__file__))
+
+# Add our project to our pythonpath, this way we don't need to type our project
+# name in our dotted import paths:
+path.append(DJANGO_ROOT)
+########## END PATH CONFIGURATION
+
+
+########## APP CONFIGURATION
+LOCAL_APPS = (
+  'cell',
+  'expt',
+  'img',
+)
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
+INSTALLED_APPS = LOCAL_APPS
+########## END APP CONFIGURATION
