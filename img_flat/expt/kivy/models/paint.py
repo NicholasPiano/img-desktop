c3529257c4c2c3fd59fba230183c16458ba8d0e8
NicholasPiano <nicholas.d.piano@gmail.com>
2015-08-03 20:42:09 +0100

    testing thrust

--- img_flat/expt/kivy/models/paint.py
+++ img_flat/expt/kivy/models/paint.py
@@ -1,10 +1,22 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.graphics import Color, Ellipse
+from kivy.core.window import Window
+from kivy.clock import Clock
 
 class PaintWidget(Widget):
   def on_touch_down(self, touch):
-    print(touch)
+    with self.canvas:
+      Color(1, 1, 0)
+      d = 30.
+      Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
 
 class PaintApp(App):
   def build(self):
     return PaintWidget()
+
+  def on_motion(self, etype, motionevent):
+    # will receive all motion events.
+    print('hello')
+
+  Window.bind(on_mouse_move=on_motion)
