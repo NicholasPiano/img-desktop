0829c2b9c8acb5cceeeeac8e4838cfda3859f16f
NicholasPiano <nicholas.d.piano@gmail.com>
2015-08-08 00:08:31 +0100

    zcmop now run out of ij directory

--- img_core/img_mvp/woot/apps/expt/models.py
+++ img_core/img_mvp/woot/apps/expt/models.py
@@ -147,7 +147,7 @@ class Experiment(models.Model):
     pipeline = os.path.join(self.pipeline_path, 'markers.cppipe')
     if key!='marker':
       pipeline = os.path.join(self.pipeline_path, 'regions.cppipe')
-    cmd = '/Applications/CellProfiler.app/Contents/MacOS/CellProfiler -c -r -i {} -o {} -p {}'.format(self.composite_path, self.cp_path, pipeline)
+    cmd = '/Applications/CellProfiler.app/Contents/MacOS/CellProfiler -c -r -i {} -o {} -p {}'.format(self.ij_path, self.cp_path, pipeline)
     subprocess.call(cmd, shell=True)
 
 class Series(models.Model):
