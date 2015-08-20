c574e4bdcad8e2778d9a748bfe8b0038fa2aa42d
NicholasPiano <nicholas.d.piano@gmail.com>
2015-08-20 18:52:06 +0100

    running browser and server in different processes

--- thrust_tests/main.py
+++
@@ -1,9 +0,0 @@
-import asyncio, pythrust
-
-loop = asyncio.get_event_loop()
-api = pythrust.API(loop)
-
-asyncio.async(api.spawn())
-asyncio.async(api.window({ 'root_url': 'http://arkaeologic.pythonanywhere.com' }).show())
-
-loop.run_forever()
