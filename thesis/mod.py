f0d72a105a6a20ef63a389b747ff0a4351493b9e
NicholasPiano <nicholas.d.piano@gmail.com>
2015-09-11 19:46:42 +0100

    mod. branching to single cell

---
+++ thesis/mod.py
@@ -0,0 +1,19 @@
+# import
+
+# load
+
+# scripts
+def make_cell(r0,r1,c0,c1):
+  pass
+
+def make_zmod(gfp_stack):
+  pass
+
+def make_zbf():
+  pass
+
+def make_zmean():
+  pass
+
+def make_zcomp():
+  pass
