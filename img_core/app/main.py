e452ca52f35f104b05b124ea4f5f1ba58a2f9774
NicholasPiano <nicholas.d.piano@gmail.com>
2015-10-07 13:33:28 +0100

    adding test site for tornado

---
+++ img_core/app/main.py
@@ -0,0 +1,5 @@
+# python wrapper to start tornado server process and thrust browser window.
+
+import 
+
+# 1.
--- img_core/app/start.js
+++ img_core/app/start.js
@@ -3,7 +3,7 @@ var path = require('path');
 
 thrust(function(err, api) {
 	// var url = 'file://'+path.resolve(__dirname, 'start.html');
-	var url = 'http://localhost:8000';
+	var url = 'http://localhost:8080/hello-django';
   var window = api.window({
     root_url: url,
     size: {
